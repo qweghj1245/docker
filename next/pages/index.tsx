e46cffc0b39a2c1ff01c7ed960fc629510057cae
mazusu <qweghj1245@gmail.com>
2020-11-25 22:57:58 +0800

    prod

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import Layout from '../components/Layout';
 
 const IndexPage = (props: any) => {
+
+  useEffect(() => {
+    fetch("http://multidocker-env-1.eba-vfascr2i.ap-northeast-1.elasticbeanstalk.com/api/post")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  })
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋 1121</h1>
@@ -11,7 +18,7 @@ const IndexPage = (props: any) => {
 }
 
 export async function getServerSideProps () {
-  const response = await fetch("/api/post").then((res) => res.json());
+  const response = await fetch("http://multidocker-env-1.eba-vfascr2i.ap-northeast-1.elasticbeanstalk.com/api/post").then((res) => res.json());
   return {
     props: { response }
   }
