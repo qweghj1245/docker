3a5089a5b92483d2c126410e837fc5157ea19609
mazusu <qweghj1245@gmail.com>
2020-11-25 23:12:05 +0800

    prod

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -12,15 +12,15 @@ const IndexPage = (props: any) => {
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋 1121</h1>
       <p>123</p>
-      <p>{props.response.message}</p>
+      <p>{props.msg}</p>
     </Layout>
   )
 }
 
 export async function getServerSideProps () {
-  const response = await fetch("http://multidocker-env-1.eba-vfascr2i.ap-northeast-1.elasticbeanstalk.com/api/post").then((res) => res.json());
+  const response = await fetch("/api/post").then((res) => res.json());
   return {
-    props: { response }
+    props: { ...response }
   }
 }
 
