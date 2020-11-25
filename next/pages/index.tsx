ae49d2a5afb555d71c07a6281c674f26f703e05e
mazusu <qweghj1245@gmail.com>
2020-11-25 21:44:19 +0800

    prod

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,19 +1,20 @@
 import Layout from '../components/Layout';
 
-const IndexPage = () => {
+const IndexPage = (props: any) => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋 1121</h1>
       <p>123</p>
+      <p>{props.response.message}</p>
     </Layout>
   )
 }
 
-// export async function getServerSideProps () {
-//   const response = await fetch("http://webserver:80/api/post").then((res) => res.json());
-//   return {
-//     props: { response }
-//   }
-// }
+export async function getServerSideProps () {
+  const response = await fetch("http://nginx:80/api/post").then((res) => res.json());
+  return {
+    props: { response }
+  }
+}
 
 export default IndexPage
